e4fa019db7d41d7565b984b69917027368285c10
Dylan Baker <dylanmbaker@yahoo.com>
2021-12-10 17:16:01 -0600

    finished the mvp instructions

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+import React from "react";
+
+import styled from "styled-components";
+
+const StyledCharacter = styled.div`
+    width: 60%;
+    margin: 0 auto;
+  background-color: #BA7F3D;
+  color: ${pr => pr.theme.secondaryColor};
+  padding: 20px;
+  margin-bottom: 20px;
+`
+
+const Character = (props) => {
+    const { character } = props;
+  return (
+      <StyledCharacter>
+          {character.name}
+      </StyledCharacter>
+  );
+}
+
+export default Character;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 // This is for the fake API. Do not delete!
 import React from "react";
-import ReactDOM from "react-dom";
+import { render} from "react-dom";
 import "./index.css";
 import App from "./App";
+
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
 
 import { worker } from "./mocks/browser";
 worker.start();
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+render(
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
+    , 
+    document.getElementById("root")
+);
